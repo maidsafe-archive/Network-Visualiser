6fbaf129877fad98da20a30b2012ec810ad9c235
krishna <krishnaof1988@gmail.com>
2014-05-30 00:28:29 +0530

    CSS issue fixes. Expand all functionality integrated.

--- client/js/controller/ApplicationCtrl.js
+++ client/js/controller/ApplicationCtrl.js
@@ -1,15 +1,23 @@
-var ApplicationCtrl = ['$scope', function($scope){
+var ApplicationCtrl = ['$scope', '$rootScope', function($scope, $rootScope){
 	
 	$scope.iconsTrayClosed = true;
 
 	$scope.vaults = []
 
+	$scope.allVaultsExpanded = false;
+
 	$scope.toggleIconsTray = function(){
 		$scope.iconsTrayClosed = !$scope.iconsTrayClosed		
 	}
 
-	for(var i=0;i<40;i++){
-		$scope.vaults.push(i*100)		
+
+	$scope.toggleExpandAllLogs = function(){
+		$scope.allVaultsExpanded = !$scope.allVaultsExpanded
+		$rootScope.$broadcast('expandVault', $scope.allVaultsExpanded)
 	}
 
+	//just for unit testing
+	for(var i=0;i<40;i++){
+		$scope.vaults.push({name:((100-i)+'demo')})		
+	}
 }];
--- client/js/controller/VaultCtrl.js
+++ client/js/controller/VaultCtrl.js
@@ -3,14 +3,22 @@ var VaultCtrl = ['$scope', function($scope){
 	$scope.logsOpen
 	$scope.progressLevel
 
+
 	$scope.updateProgress = function(progress){
 		$scope.progressLevel = {width: (progress+'%')}		
 	}
 
-	$scope.toggleStateIcon = function(){
-		$scope.logsOpen = !$scope.logsOpen		
+	$scope.toggleVaultLogs = function(expand){
+		$scope.logsOpen = expand?expand:!$scope.logsOpen		
 		$scope.stateIcon = $scope.logsOpen?"arrow-up.png":"info.png"
 	}
+
+	$scope.$on('expandVault', function(e, v){				
+		//If the node is already in the expanded/collapsed state then, no action is needed to be performed 
+		if(v == $scope.logsOpen)
+			 return;
+		$scope.toggleVaultLogs(v)
+	})
 
 	//initialize the controller
 	$scope.updateProgress(0)//update progress to zero initially
