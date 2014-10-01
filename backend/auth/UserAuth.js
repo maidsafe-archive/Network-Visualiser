a802e74cd010601c141669d56296dae35bdd1b75
krishn <krishnakumarp@karyatech.com>
2014-10-01 13:14:13 +0530

    utils and auth jscs passed

--- backend/auth/UserAuth.js
+++ backend/auth/UserAuth.js
@@ -3,12 +3,11 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var GitHubStrategy = require('passport-github').Strategy;
 var fs = require('fs');
 var path = require('path');
-
 var config = require('./../../Config.js');
 var utils = require('./../maidsafe/utils.js');
 var gAuth;
 var needsAuth = true;
-
+var googleOauthScope = {scope: ['https://www.googleapis.com/auth/userinfo.email']};
 exports.initAuth = function(mailerCallback) {
   var fileContent;
   try {
@@ -18,62 +17,47 @@ exports.initAuth = function(mailerCallback) {
     needsAuth = false;
     return needsAuth;
   }
-
   gAuth = JSON.parse(fileContent);
-
   passport.use(new GoogleStrategy({
-      clientID: gAuth.GOOGLE.CLIENT_ID,
-      clientSecret: gAuth.GOOGLE.CLIENT_SECRET,
-      callbackURL: gAuth.GOOGLE.CALLBACK
-    },
-    function(accessToken, refreshToken, profile, done) {
-      process.nextTick(function() {
-        return done(null, { email: profile._json.email });
-      });
-    }
-  ));
-
+    clientID: gAuth.GOOGLE.CLIENT_ID,
+    clientSecret: gAuth.GOOGLE.CLIENT_SECRET,
+    callbackURL: gAuth.GOOGLE.CALLBACK
+  }, function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function() {
+      return done(null, {email: profile._json.email});
+    });
+  }));
   passport.use(new GitHubStrategy({
-      clientID: gAuth.GITHUB.CLIENT_ID,
-      clientSecret: gAuth.GITHUB.CLIENT_SECRET,
-      callbackURL: gAuth.GITHUB.CALLBACK
-    },
-    function(accessToken, refreshToken, profile, done) {
-      process.nextTick(function() {
-        return done(null, { email: profile._json.login + '@github' });
-      });
-    }
-  ));
-
+    clientID: gAuth.GITHUB.CLIENT_ID,
+    clientSecret: gAuth.GITHUB.CLIENT_SECRET,
+    callbackURL: gAuth.GITHUB.CALLBACK
+  }, function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function() {
+      return done(null, {email: profile._json.login + '@github'});
+    });
+  }));
   passport.serializeUser(function(user, done) {
     done(null, user);
   });
-
   passport.deserializeUser(function(obj, done) {
     done(null, obj);
   });
-
   mailerCallback(gAuth.MAIL_ID, gAuth.MAIL_PASS);
-
   return needsAuth;
 };
-
 exports.configureAuth = function(server) {
   if (!needsAuth) {
     return;
   }
-
   server.use(passport.initialize());
   server.use(passport.session());
 };
-
 var setUserInfo = function(req, res, next) {
   var userInfo = {
     isAuthenticated: false,
     isMaidSafeUser: false,
     mailAddress: ''
   };
-
   if (!needsAuth) {
     userInfo = {
       isAuthenticated: true,
@@ -98,48 +82,38 @@ var setUserInfo = function(req, res, next) {
       }
     }
   }
-
   req._userInfo = userInfo;
   return next();
 };
-
 exports.appendUserInfo = setUserInfo;
-
 exports.setupAuthCallbacks = function(server) {
   if (!needsAuth) {
     return;
   }
-
   server.get('/auth', setUserInfo, function(req, res) {
     if (req.isAuthenticated() && !req._userInfo.isAuthenticated) {
       res.redirect('/');
       return;
     }
-
     res.render('sessions', {
       userInfo: req._userInfo,
       socketPort: config.Constants.socketPort
     });
   });
-
-  server.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.email'] }), function(req, res) {
+  server.get('/auth/google', passport.authenticate('google', googleOauthScope), function(req, res) {
     // The request will be redirected to Google for authentication, so this
     // function will not be called.
   });
-
   server.get('/auth/github', passport.authenticate('github'), function(req, res) {
     // The request will be redirected to Github for authentication, so this
     // function will not be called.
   });
-
-  server.get('/googlecallback', passport.authenticate('google', { failureRedirect: '/' }), function(req, res) {
+  server.get('/googlecallback', passport.authenticate('google', {failureRedirect: '/'}), function(req, res) {
     res.redirect('/auth');
   });
-
-  server.get('/githubcallback', passport.authenticate('github', { failureRedirect: '/' }), function(req, res) {
+  server.get('/githubcallback', passport.authenticate('github', {failureRedirect: '/'}), function(req, res) {
     res.redirect('/auth');
   });
-
   server.get('/auth/logout', function(req, res) {
     req.logout();
     res.redirect('/');
--- backend/maidsafe/utils.js
+++ backend/maidsafe/utils.js
@@ -1,13 +1,13 @@
 var config = require('./../../Config.js');
-var fs = require('fs');
-
+/*jshint sub:true*/
 exports.isValid = function(log) {
-  var isValid = log.vault_id && log.action_id && log.persona_id;
+  var isValid = log['vault_id'] && log['action_id'] && log['persona_id'];
   if (!log.hasOwnProperty('session_id')) {
     isValid = false;
   }
   return isValid;
 };
+/*jshint sub:false*/
 exports.formatDate = function(log) {
   try {
     if (log.ts) {
@@ -23,21 +23,26 @@ exports.formatDate = function(log) {
   }
   return true;
 };
+/* jshint unused:false */
+/* jshint forin:false */
 exports.isEmptyObject = function(object) {
   for (var i in object) {
     return false;
   }
   return true;
 };
+/* jshint unused:true */
+/* jshint forin:true */
 exports.transformVaultId = function(vaultId) {
   return (vaultId.indexOf('..') > -1) ? vaultId.replace('..', '_') : vaultId.replace('_', '..');
 };
 exports.decodeData = function(str) {
-  str = new Buffer(str, "base64").toString("utf8");
+  str = new Buffer(str, 'base64').toString('utf8');
   return new Buffer(str).toString('hex');
 };
+/*jshint sub:true*/
 exports.isPageRequestValid = function(criteria) {
-  if (criteria.vault_id) {
+  if (criteria['vault_id']) {
     if (criteria.page) {
       try {
         criteria.page = parseInt(criteria.page);
@@ -47,7 +52,6 @@ exports.isPageRequestValid = function(criteria) {
     } else {
       criteria.page = 0;
     }
-
     if (criteria.max) {
       try {
         criteria.max = parseInt(criteria.max);
@@ -61,12 +65,13 @@ exports.isPageRequestValid = function(criteria) {
   }
   return false;
 };
+/*jshint sub:false*/
 exports.transformQuery = function(query) {
   for (var key in query) {
-    if (key == 'ts') {
-      query[key] = { "$gt": query[key] }; //works fine only ISO string - query is done based on ISO string
-    } else { //For like operation on strings
-      query[key] = new RegExp(query[key], "i");
+    if (key === 'ts') {
+      query[key] = {'$gt': query[key]}; // works fine only ISO string - query is done based on ISO string
+    } else { // For like operation on strings
+      query[key] = new RegExp(query[key], 'i');
     }
   }
   return query;
@@ -76,11 +81,13 @@ exports.ensureUniqueDocInMongo = function(dbConn, collectionName, fieldName) {
     if (coll) {
       var ob = {};
       ob[fieldName] = 1;
-      coll.ensureIndex(ob, { unique: true, dropDups: true }, function(errorUpdate, writeSuccess) {
+      /* jshint unused:false */
+      coll.ensureIndex(ob, {unique: true, dropDups: true}, function(errorUpdate, writeSuccess) {
         if (errorUpdate) {
           console.log(errorUpdate);
         }
       });
+      /* jshint unused:true */
     }
   });
 };
@@ -89,7 +96,7 @@ exports.generateRandomSessionIdString = function(prefix) {
   var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = (d + Math.random() * 16) % 16 | 0;
     d = Math.floor(d / 16);
-    return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+    return (c === 'x' ? r : (r & 0x7 | 0x8)).toString(16);
   });
   return prefix == null ? uuid : prefix + uuid;
 };
@@ -99,9 +106,11 @@ exports.hasSessionName = function(criteria) {
 exports.filterSessionVaultNames = function(sessionId, dbConnName, collections) {
   var sessionVaultNames = [];
   for (var i in collections) {
-    var collName = collections[i].name.replace(dbConnName + '.', '');
-    if (collName.indexOf(sessionId) == 0) {
-      sessionVaultNames.push(collName);
+    if (collections[i]) {
+      var collName = collections[i].name.replace(dbConnName + '.', '');
+      if (collName.indexOf(sessionId) === 0) {
+        sessionVaultNames.push(collName);
+      }
     }
   }
   return sessionVaultNames;
