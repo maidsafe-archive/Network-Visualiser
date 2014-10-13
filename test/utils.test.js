b507bf075a4d31958aa060fe2168ba35e0b44df2
krishn <krishnakumarp@karyatech.com>
2014-10-13 09:05:15 +0530

    utils connection map

--- test/utils.test.js
+++ test/utils.test.js
@@ -12,9 +12,63 @@ describe('Utils Test Suite', function() {
     utils.isEmptyObject([ 1 ]).should.be.false;
   });
   it('Should be able to transform old format to cameCase for log', function() {
-    var log = { 'vault_id': 'aaa..bbb', 'session_id': 'gjhjhjhfg80987676', 'action_id': 0, 'vaule1': '' };
+    var log = { 'vault_id': 'aaa..bbb', 'session_id': 'gjhjhjhfg80987676', 'action_id': 0, 'value1': '' };
     utils.prepareLogModel(log);
     log.hasOwnProperty('vaultId').should.be.ok;
     log.hasOwnProperty('vault_id').should.not.be.ok;
+  });
+  it('Validate log - Should not thrown an error for valid log', function(){
+    var log = {
+      vaultId: 'aaa..bbb', sessionId: 'gjhjhjhfg80987676',
+      actionId: 10, personaId:10 , value1: '787987',
+      'ts': '2014-10-10 03:32:09.350'
+    };
+    should(utils.validateRequestLogModel(log)).not.be.ok;
+  });
+  it('Validate log - Should thrown an error for invalid action id', function(){
+    var log = { vaultId: 'aaa..bbb', sessionId: 'gjhjhjhfg80987676', actionId: 20, personaId:10 ,value1: '787987' ,  'ts': '2014-10-10 03:32:09.350'};
+    should(utils.validateRequestLogModel(log)).be.ok;
+  });
+  it('Validate log - Should thrown an error for empty value1', function(){
+    var log = { vaultId: 'aaa..bbb', sessionId: 'gjhjhjhfg80987676', actionId: 0, personaId:10 ,value1: '',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+  });
+  it('Validate Log - action Id must be a proper integer', function(){
+    var log = { vaultId: 'aaa..bbb', sessionId: 'gjhjhjhfg80987676', actionId: 'sdf', personaId:10 ,value1: '787987',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+  });
+  it('Validate Log - Persona Id must be a proper integer', function(){
+    var log = { vaultId: 'aaa..bbb', sessionId: 'gjhjhjhfg80987676', actionId: 10, personaId: 'sdf' ,value1: '787987',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+  });
+  it('Validate Log - Value1 must be a integer for action Id 17', function(){
+    var log = { vaultId: 'aaa..bbb', sessionId: 'gjhjhjhfg80987676', actionId: 17, personaId: 10 ,value1: 'g89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+  });
+  it('Validate Log - Should be able to validate for required fields', function(){
+    var log = { vaultId: 'aaa..bbb', sessionId: 'gjhjhjhfg80987676', actionId: 17, personaId: 10 ,value1: '89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).not.be.ok;
+    log = { vaultId: 'aaa..bbb', sessionId: 'gjhjhjhfg80987676', actionId: 17, personaId: 10 ,value1: '89' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+  });
+  it('Validate Log - should be validate if vaultId parameter', function(){
+    var log = { vaultId: '', sessionId: 'gjhjhjhfg80987676', actionId: 17, personaId: 10 ,value1: '89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+    log = { vaultId: null, sessionId: 'gjhjhjhfg80987676', actionId: 17, personaId: 10 ,value1: '89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+    log = { sessionId: 'gjhjhjhfg80987676', actionId: 17, personaId: 10 ,value1: '89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+    log = { vaultId: 'sdsd', sessionId: 'gjhjhjhfg80987676', actionId: 17, personaId: 10 ,value1: '89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).not.be.ok;
+  });
+  it('Validate Log - should be validate if sessionId parameter', function(){
+    var log = { vaultId: 'sds', sessionId: '', actionId: 17, personaId: 10 ,value1: '89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+    log = { vaultId: null, sessionId: null, actionId: 17, personaId: 10 ,value1: '89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+    log = { vaultId: 'gjhjhjhfg80987676', actionId: 17, personaId: 10 ,value1: '89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).be.ok;
+    log = { vaultId: 'sdsd', sessionId: 'gjhjhjhfg80987676', actionId: 17, personaId: 10 ,value1: '89',  'ts': '2014-10-10 03:32:09.350' };
+    should(utils.validateRequestLogModel(log)).not.be.ok;
   });
 });
