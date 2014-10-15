603f74d44b12b6a01682969c1d416ddf23b246cd
krishn <krishnakumarp@karyatech.com>
2014-10-15 15:11:51 +0530

    Saving action connection log implemented

--- backend/mongo/connection_map/ActualConnection.js
+++ backend/mongo/connection_map/ActualConnection.js
@@ -1,11 +1,30 @@
 var mongoose = require('mongoose');
-
-var ActualConnection = function(dbCon) {
+module.exports = function(dbCon) {
   var instance = this;
-  var saveActualLog = function(sessionId, log) {
-
+  var COLLECTION_NAME_SUFFIX = '_actual_connection';
+  var GeneralHandler = function(promise) {
+    return function(err, doc) {
+      if (err) {
+        promise.error(err);
+        return;
+      }
+      promise.complete(doc);
+    };
+  };
+  var saveActualLog = function(log, callback) {
+    var promise = new mongoose.Promise();
+    if (callback) {
+      promise.addBack(callback);
+    }
+    dbCon.db.collection(log.sessionId + COLLECTION_NAME_SUFFIX, function(err, collection) {
+      if (err) {
+        promise.error(err);
+        return;
+      }
+      collection.save(log, new GeneralHandler(promise));
+    });
+    return promise;
   };
   instance.save = saveActualLog;
   return instance;
 };
-module.exports = ActualConnection;
--- backend/mongo/connection_map/connectionmapbridge.js
+++ backend/mongo/connection_map/connectionmapbridge.js
@@ -1,16 +1,15 @@
 var mongoose = require('mongoose');
+var ActualConnectionHandler = require('./ActualConnection');
 var MongoBridge = function() {
   var instance = this;
   var dbCon;
+  var actualConnection;
   instance.setDB = function(db) {
     dbCon = db;
+    actualConnection = new ActualConnectionHandler(db);
   };
   instance.addActualLog = function(log, callback) {
-    var promise = new mongoose.Promise();
-    if (callback) {
-      promise.addBack(callback);
-    }
-    return promise;
+    return actualConnection.save(log, callback);
   };
   instance.updateExpected = function(log, promise) {
     // TODO handle and save the log
