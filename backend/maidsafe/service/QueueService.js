5711624dc25b71dafc5503ce37d57076b3e5cf54
krishn <krishnakumarp@karyatech.com>
2014-10-16 16:03:45 +0530

    Separate Queues are maintained for each session

--- backend/maidsafe/service/QueueService.js
+++ backend/maidsafe/service/QueueService.js
@@ -1,49 +1,61 @@
 var sq = require('simplequeue');
-var queue = sq.createQueue();
-var msg;
-var STATE = {
-  START: 0,
-  STOP: 1
-};
-var currentState = STATE.STOP;
+var queuePool = {};
 var consumer;
-var DoneCallback;
-var broadcast;
-var onReceived;
-broadcast = function() {
-  msg = queue.getMessageSync();
-  if (msg) {
-    consumer(msg, new DoneCallback());
-  }
-  currentState = STATE.STOP;
-};
-DoneCallback = function() {
-  var timerId;
-  var timerDuration = 2000;
-  var completed = false;
-  var done = function() {
-    clearTimeout(timerId);
-    if (!completed) {
-      completed = true;
+var SessionQueue = function() {
+  var msg;
+  var queue = sq.createQueue();
+  var STATE = {
+    START: 0,
+    STOP: 1
+  };
+  var currentState = STATE.STOP;
+  var DoneCallback;
+  var broadcast;
+  var onReceived;
+  broadcast = function() {
+    msg = queue.getMessageSync();
+    if (msg) {
+      consumer(msg, new DoneCallback());
+    }
+    currentState = STATE.STOP;
+  };
+  DoneCallback = function() {
+    var timerId;
+    var timerDuration = 30000;
+    var completed = false;
+    var done = function() {
+      clearTimeout(timerId);
+      if (!completed) {
+        completed = true;
+        broadcast();
+      }
+    };
+    timerId = setTimeout(function() {
+      done();
+      console.error('%s Queue was restarted forcefully - done callback was not completed in 2000ms',
+        new Date().toISOString());
+    }, timerDuration);
+    return done;
+  };
+  onReceived = function() {
+    if (currentState === STATE.STOP) {
+      currentState = STATE.START;
       broadcast();
     }
   };
-  timerId = setTimeout(function() {
-    done();
-    console.error('%s Queue was restarted forcefully - done callback was not completed in 2000ms',
-      new Date().toISOString());
-  }, timerDuration);
-  return done;
-};
-onReceived = function() {
-  if (currentState === STATE.STOP) {
-    currentState = STATE.START;
-    broadcast();
+  this.pushToQueue  = function(log) {
+    queue.putMessage(log);
+    onReceived();
+  };
+}
+exports.pushToQueue = function(log) {
+  if (!queuePool[log.sessionId]) {
+    queuePool[log.sessionId] = new SessionQueue();
   }
+  queuePool[log.sessionId].pushToQueue(log);
 };
-exports.pushToQueue = function(obj) {
-  queue.putMessage(obj);
-  onReceived();
+exports.deleteQueue = function(sessionId) {
+  delete queuePool[sessionId];
 };
 exports.subscribe = function(con) {
   consumer = con;
