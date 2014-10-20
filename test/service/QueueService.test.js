70b95f24d05e6ea73453556fe9f087d9c18ef30a
krishn <krishnakumarp@karyatech.com>
2014-10-20 14:59:14 +0530

    Failing FIFO Queue test

--- test/service/QueueService.test.js
+++ test/service/QueueService.test.js
@@ -12,12 +12,12 @@ describe('QueueService', function() {
       valueOne: 'sd',
       ts: '2014-10-10 03:32:09.350'
     };
-//    QueueService.subscribe(function(logReceived, callBack) {
-//      logReceived.should.be.exactly(log);
+    QueueService.subscribe(function(logReceived, callBack) {
+      logReceived.should.be.exactly(log);
 //      callBack();
-//      QueueService.deleteQueue(log.sessionId).should.be.ok;
+      QueueService.deleteQueue(log.sessionId).should.be.ok;
       done();
-//    });
-//    QueueService.pushToQueue(log);
+    });
+    QueueService.pushToQueue(log);
   });
 });
