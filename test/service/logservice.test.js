9a6f38410c6763efb8e2925e262f721500738d54
KrishnaIndia <krishnaof1988@gmail.com>
2014-10-30 01:47:48 +0530

    MAID-352 test cases updated

--- test/service/logservice.test.js
+++ test/service/logservice.test.js
@@ -186,16 +186,52 @@ describe('LogService', function() {
     };
     logService.saveLog(req, new mock.Response(done, assert));
   });
-  it('Should be able to get snapshot of connectionmap', function(done) {
+  it('Should be able to get connection map snapshot', function(done) {
     var mockCallback = function() {
     };
     var logSaved = function() {
-      connectionMapService.snapshot(serviceHelper.getSessionName(), null, function(err, data) {
-        if (!err) {
+      connectionMapService.snapshot(
+        serviceHelper.getSessionName(), '2014-10-12T12:00:00.000Z', function(err, data) {
+          should(err).not.be.ok;
+          should(data).be.ok;
+          done();
+        });
+    };
+    var addActualLog = function() {
+      req = new mock.Request();
+      req.body = {
+        vaultId: '', actionId: 19, sessionId: serviceHelper.getSessionId(),
+        ts: '2014-10-12T12:00:00.000Z', valueOne: {
+          vaultAdded: 'sasd..asd',
+          vaultRemoved: 'asd..asd'
+        }
+      };
+      logService.saveLog(req, new mock.Response(logSaved, mockCallback));
+    };
+    var addStartLog = function() {
+      req = new mock.Request();
+      req.body = {
+        vaultId: '', actionId: 19, sessionId: serviceHelper.getSessionId(),
+        ts: '2014-10-12T12:00:00.000Z', valueOne: {
+          vaultAdded: 'sasd..asd',
+          vaultRemoved: 'asd..asd'
+        }
+      };
+      logService.saveLog(req, new mock.Response(addActualLog, mockCallback));
+    };
+    addStartLog();
+  });
+  it('Should be able to get connection map diffs', function(done) {
+    var mockCallback = function() {
+    };
+    var logSaved = function() {
+      connectionMapService.connectionMapDiff (
+        serviceHelper.getSessionName(), '2014-10-12T11:00:00.000Z', '2014-10-12T12:00:00.000Z', function(err, data) {
+          should(err).not.be.ok;
           should(data).be.ok;
           done();
         }
-      });
+      );
     };
     var addActualLog = function() {
       req = new mock.Request();
