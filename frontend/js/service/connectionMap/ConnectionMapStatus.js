668266ba8986ed3cd33541bc35303031937f28fd
KrishnaIndia <krishnaof1988@gmail.com>
2014-10-31 00:51:48 +0530

    Services implemented for computing the actual and expected connection status

---
+++ frontend/js/service/connectionMap/ConnectionMapStatus.js
@@ -0,0 +1,37 @@
+/* global window:false */
+window.ConnectionMapStatus = [ 'expectedConnectionHelper', function(expectedConnectionHelper) {
+  var instance = this;
+  var expectedConnections = {};
+  var actualConnections = {};
+  var setExpected = function(data) {
+    expectedConnections = data;
+  };
+  var setActual = function(data) {
+    actualConnections = data;
+  };
+  var updateExpected = function(diffs) {
+    for (var index in diffs) {
+      if (diffs[index]) {
+        expectedConnections[diffs[index].vaultId] = diffs[index].closestVaults;
+      }
+    }
+  };
+  var updateActual = function(data) {
+    for (var index in data) {
+      if (data[index]) {
+        actualConnections[data[index].vaultId] = data[index].closestVaults;
+      }
+    }
+  };
+  var computeExpected = function(log) {
+    var compute = (log.actionId === 0) ? expectedConnectionHelper.computeExpectedOnStart
+      : expectedConnectionHelper.computeExpectedOnStart;
+    updateExpected(compute(log, expectedConnections));
+  };
+  instance.setActualStatus = setActual;
+  instance.setExpectedStatus = setExpected;
+  instance.updateExpected = updateExpected;
+  instance.updateActual = updateActual;
+  instance.computeExpectedOnStart = computeExpected;
+  return instance;
+} ];
