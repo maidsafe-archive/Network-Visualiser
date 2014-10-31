61bc2ec1fdfa12b73d6df7e22a4d35da4a295d49
KrishnaIndia <krishnaof1988@gmail.com>
2014-10-31 16:52:45 +0530

    connectionmap.ejs merged from branch

---
+++ frontend/js/reactjs/jsx-output/connectionmap.js
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+window.ConnectionMap = React.createClass({displayName: 'ConnectionMap',
+  getInitialState: function() {
+    return {connectionMap: null};
+  },
+  componentDidMount: function() {
+    this.props.scope.registerReactComponent(this);
+    this.setState({connectionMap: new ConnectionMapBuilder(null, 'conMap')});
+  },
+  render: function() {
+    if (this.state.connectionMap) {
+      this.state.connectionMap.drawConnections(this.props.scope.connections.slice(0));
+    }
+    return React.DOM.div({id: 'conMap'});
+  }
+});
